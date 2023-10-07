89c0a831c65397fb2331cd87ea6876a64be9e491
J-DVbk <j-d.vbk@gmx.de>
2023-10-07 11:56:01 +0200

    Added TextmeshPro

--- Assets/Scripts/GameManagement/TirednessManager.cs
+++ Assets/Scripts/GameManagement/TirednessManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class TirednessManager : MonoBehaviour
 {
+    private static TirednessManager instance;
+    public static TirednessManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<TirednessManager>();
+            return instance;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
