2b20bd4360e07c443e56ef3786250a1246d99191
Leonie Straßer <leonie.strasser@yahoo.de>
2023-10-08 13:00:49 +0200

    CharacterAnim

--- Assets/Scripts/Character/CharacterVisualization.cs
+++ Assets/Scripts/Character/CharacterVisualization.cs
@@ -9,17 +9,20 @@ public class CharacterVisualization : MonoBehaviour
     [SerializeField] private SpriteRenderer FaceRenderer;
     [SerializeField] private SpriteRenderer HairRenderer;
     [SerializeField] private SpriteRenderer HeadRenderer;
-    
+
     [Header("Sprites")]
     [SerializeField] private Sprite FaceAwake;
     [SerializeField] private Sprite FaceTired;
     [SerializeField] private Sprite FaceVeryTired;
-    
+
     [SerializeField] private Sprite Hair;
     [SerializeField] private Sprite HairMessy;
-    
+
     [SerializeField] private Sprite Head;
     [SerializeField] private Sprite HeadPale;
+
+    [Header("Animation")]
+    [SerializeField] private Animator anim;
 
     [Header("Other Settings")]
     [SerializeField] private float FaceChangeCooldownDuration = 1f;
@@ -43,17 +46,26 @@ public class CharacterVisualization : MonoBehaviour
 
     public void UpdateFace(float _tiredness)
     {
-        if(!FaceChangeAllowed)
+        if (!FaceChangeAllowed)
             return;
-        
+
         if (_tiredness < 0.33f)
+        {
             ChangeFaceState(FaceState.Awake);
-        
+            SetAnimationState(_tiredness);
+        }
+
+
         else if (_tiredness > 0.33f && _tiredness < 0.66f)
+        {
             ChangeFaceState(FaceState.Tired);
-        
+            SetAnimationState(_tiredness);
+        }
         else
+        {
             ChangeFaceState(FaceState.VeryTired);
+            SetAnimationState(_tiredness);
+        }
     }
 
     private void ChangeFaceState(FaceState _newFaceState)
@@ -89,5 +101,10 @@ public class CharacterVisualization : MonoBehaviour
     private void OnDestroy()
     {
         Bubble.OnTirednessChanged -= UpdateFace;
+    }
+
+    private void SetAnimationState(float tiredness)
+    {
+        anim.SetFloat("tired", tiredness);
     }
 }
